b0078ca0ab7b07cfc0e5c24dd19027d746653d76
Resul Alkan <ralkan@Resuls-iMac.local>
2017-11-02 00:39:07 +0300

    Added floating-button and section-title widgets

---
+++ includes/widgets/floating-button.php
@@ -0,0 +1,361 @@
+<?php
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Button Widget
+ */
+class Widget_Floating_Button extends Widget_Base {
+
+	/**
+	 * Retrieve button widget name.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'floating_button';
+	}
+
+	/**
+	 * Retrieve button widget title.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Floating Button', 'elementor' );
+	}
+
+	/**
+	 * Retrieve button widget icon.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-button';
+	}
+
+	/**
+	 * Register button widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_button',
+			[
+				'label' => __( 'Floating Button', 'elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'button_type',
+			[
+				'label' => __( 'Type', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'floating',
+				'options' => [
+					'floating' => __( 'Default', 'elementor' ),
+					'graylight' => __( 'Gri', 'elementor' ),
+				],
+				'selector' => '{{WRAPPER}} a.cpblm-button, {{WRAPPER}} .cpblm-button',
+			]
+		);
+
+		$this->add_control(
+			'text',
+			[
+				'label' => __( 'Text', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => __( 'Click me', 'elementor' ),
+				'placeholder' => __( 'Click me', 'elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'link',
+			[
+				'label' => __( 'Link', 'elementor' ),
+				'type' => Controls_Manager::URL,
+				'placeholder' => 'http://your-link.com',
+				'default' => [
+					'url' => '#',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'align',
+			[
+				'label' => __( 'Alignment', 'elementor' ),
+				'type' => Controls_Manager::CHOOSE,
+				'options' => [
+					'left'    => [
+						'title' => __( 'Left', 'elementor' ),
+						'icon' => 'fa fa-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'elementor' ),
+						'icon' => 'fa fa-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'elementor' ),
+						'icon' => 'fa fa-align-right',
+					],
+					'justify' => [
+						'title' => __( 'Justified', 'elementor' ),
+						'icon' => 'fa fa-align-justify',
+					],
+				],
+				'prefix_class' => 'elementor%s-align-',
+				'default' => '',
+			]
+		);
+
+		$this->add_control(
+			'icon',
+			[
+				'label' => __( 'Icon', 'elementor' ),
+				'type' => Controls_Manager::ICON,
+				'label_block' => true,
+				'default' => '',
+			]
+		);
+
+		$this->add_control(
+			'icon_align',
+			[
+				'label' => __( 'Icon Position', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'left',
+				'options' => [
+					'left' => __( 'Before', 'elementor' ),
+					'right' => __( 'After', 'elementor' ),
+				],
+				'condition' => [
+					'icon!' => '',
+				],
+			]
+		);
+
+		$this->add_control(
+			'icon_indent',
+			[
+				'label' => __( 'Icon Spacing', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'max' => 50,
+					],
+				],
+				'condition' => [
+					'icon!' => '',
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button .elementor-align-icon-right' => 'margin-left: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .elementor-button .elementor-align-icon-left' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'view',
+			[
+				'label' => __( 'View', 'elementor' ),
+				'type' => Controls_Manager::HIDDEN,
+				'default' => 'traditional',
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Floating Button', 'elementor' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'elementor' ),
+				'selector' => '{{WRAPPER}} a.cpblm-button, {{WRAPPER}} .cpblm-button',
+        'fields_options' => [
+          'typography' => [
+            'default' => 'custom',
+          ],
+          'text_transform' => [
+            'default' => 'uppercase'
+          ],
+          'font_family' => [
+            'default' => 'Source Sans Pro'
+          ],
+          'font_weight' => [
+            'default' => ''
+          ]
+        ]
+			]
+		);
+
+		$this->start_controls_tabs( 'tabs_button_style' );
+
+		$this->start_controls_tab(
+			'tab_button_normal',
+			[
+				'label' => __( 'Normal', 'elementor' ),
+			]
+		);
+
+      $this->add_control(
+        'button_text_color',
+        [
+          'label' => __( 'Text Color', 'elementor' ),
+          'type' => Controls_Manager::COLOR,
+          'default' => '',
+          'selectors' => [
+            '{{WRAPPER}} a.cpblm-button, {{WRAPPER}} .cpblm-button' => 'color: {{VALUE}};',
+          ],
+        ]
+      );
+
+      $this->add_control(
+        'background_color',
+        [
+          'label' => __( 'Background Color', 'elementor' ),
+          'type' => Controls_Manager::COLOR,
+          'selectors' => [
+            '{{WRAPPER}} a.cpblm-button, {{WRAPPER}} .cpblm-button' => 'background-color: {{VALUE}};',
+          ],
+        ]
+      );
+
+		$this->end_controls_tab();
+
+		$this->start_controls_tab(
+			'tab_button_hover',
+			[
+				'label' => __( 'Hover', 'elementor' ),
+			]
+		);
+
+      $this->add_control(
+        'button_background_hover_color',
+        [
+          'label' => __( 'Background Color', 'elementor' ),
+          'type' => Controls_Manager::COLOR,
+          'selectors' => [
+            '{{WRAPPER}} a.cpblm-button:hover, {{WRAPPER}} .cpblm-button:hover' => 'background-color: {{VALUE}};',
+          ],
+        ]
+      );
+
+		$this->end_controls_tab();
+
+		$this->end_controls_tabs();
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render button widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings();
+
+		$this->add_render_attribute( 'wrapper', 'class', 'elementor-button-wrapper' );
+
+		if ( ! empty( $settings['link']['url'] ) ) {
+			$this->add_render_attribute( 'button', 'href', $settings['link']['url'] );
+			$this->add_render_attribute( 'button', 'class', 'elementor-button-link' );
+
+			if ( $settings['link']['is_external'] ) {
+				$this->add_render_attribute( 'button', 'target', '_blank' );
+			}
+
+			if ( $settings['link']['nofollow'] ) {
+				$this->add_render_attribute( 'button', 'rel', 'nofollow' );
+			}
+		}
+
+		$this->add_render_attribute( 'button', 'class', 'cpblm-button btn button button--'.$settings['button_type'] );
+
+		if ( ! empty( $settings['size'] ) ) {
+			$this->add_render_attribute( 'button', 'class', 'elementor-size-' . $settings['size'] );
+		}
+
+		if ( $settings['hover_animation'] ) {
+			$this->add_render_attribute( 'button', 'class', 'elementor-animation-' . $settings['hover_animation'] );
+		}
+
+		?>
+		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
+			<a <?php echo $this->get_render_attribute_string( 'button' ); ?>>
+				<?php $this->render_text(); ?>
+			</a>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render button widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @access protected
+	 */
+	protected function _content_template() {
+		?>
+		<div class="elementor-button-wrapper">
+			<a class="cpblm-button btn button button--{{ settings.button_type }}" href="{{ settings.link.url }}">
+				<span class="elementor-button-content-wrapper">
+					<# if ( settings.icon ) { #>
+					<span class="elementor-button-icon elementor-align-icon-{{ settings.icon_align }}">
+						<i class="{{ settings.icon }}"></i>
+					</span>
+					<# } #>
+					<span class="elementor-button-text">{{{ settings.text }}}</span>
+				</span>
+			</a>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render button widget text.
+	 *
+	 * @access protected
+	 */
+	protected function render_text() {
+		$settings = $this->get_settings();
+		$this->add_render_attribute( 'content-wrapper', 'class', 'elementor-button-content-wrapper' );
+		$this->add_render_attribute( 'icon-align', 'class', 'elementor-align-icon-' . $settings['icon_align'] );
+		$this->add_render_attribute( 'icon-align', 'class', 'elementor-button-icon' );
+		?>
+		<span <?php echo $this->get_render_attribute_string( 'content-wrapper' ); ?>>
+			<?php if ( ! empty( $settings['icon'] ) ) : ?>
+			<span <?php echo $this->get_render_attribute_string( 'icon-align' ); ?>>
+				<i class="<?php echo esc_attr( $settings['icon'] ); ?>"></i>
+			</span>
+			<?php endif; ?>
+			<span class="elementor-button-text"><?php echo $settings['text']; ?></span>
+		</span>
+		<?php
+	}
+}
